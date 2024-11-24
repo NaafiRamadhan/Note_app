56c17371610a83516940211042088e8a7d749b16
NaafiRamadhan <zidannaaafi@gmail.com>
2024-11-24 20:26:48 +0700

    first

---
+++ app/Filament/Resources/CatatanResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CatatanResource\Pages;
+use App\Filament\Resources\CatatanResource\RelationManagers;
+use App\Models\Catatan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CatatanResource extends Resource
+{
+    protected static ?string $model = Catatan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCatatans::route('/'),
+            'create' => Pages\CreateCatatan::route('/create'),
+            'edit' => Pages\EditCatatan::route('/{record}/edit'),
+        ];
+    }
+}
